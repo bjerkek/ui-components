67e2e3b1dfaa0ebd0c7bed6b2a033858385adc54
Jens-Christian Bjerkek <jens-christian.bjerkek@sparebank1.no>
2021-01-08 21:07:51 +0100

    More readable config

--- babel.config.js
+++ babel.config.js
@@ -1,33 +1,36 @@
 module.exports = api => {
   api.cache(true)
 
-  const { NODE_ENV } = process.env // test, development, production
-  const inTest = NODE_ENV === 'test'
-
-  let presets = [
-    '@babel/preset-typescript', [
-      '@babel/preset-env', {
-        targets: ['last 2 versions', 'ie 11'],
-        // modules: api.env('es') ? false : 'commonjs'
-        modules: false
-      }
-    ]
+  const presets = [
+    '@babel/preset-typescript'
   ]
-
-  if (inTest) {
-    presets = [
-      '@babel/preset-typescript',
-      '@babel/preset-env'
-    ]
-  }
 
   const plugins = [
-    '@babel/plugin-transform-runtime',
-    inTest && '@babel/plugin-proposal-class-properties'
+    '@babel/plugin-transform-runtime'
   ]
+
+  const env = {
+    production: {
+      presets: [
+        '@babel/preset-env', {
+          targets: ['last 2 versions', 'ie 11'],
+          modules: false
+        }
+      ]
+    },
+    test: {
+      presets: [
+        '@babel/preset-env'
+      ],
+      plugins: [
+        '@babel/plugin-proposal-class-properties'
+      ]
+    }
+  }
 
   return {
     presets,
-    plugins
+    plugins,
+    env
   }
 }

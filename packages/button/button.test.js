01962b28de8baaa9fa02e9859faf817878199f12
Jens-Christian Bjerkek <jens-christian.bjerkek@sparebank1.no>
2021-01-22 20:13:19 +0100

    Simplifying tests

--- packages/button/button.test.js
+++ packages/button/button.test.js
@@ -3,35 +3,30 @@ import { Button } from './button'
 
 describe('Button', () => {
   let buttonEl
+  let button
 
   beforeAll(() => {
     window.customElements.define('ui-button', Button)
     buttonEl = document.createElement('ui-button')
     buttonEl.innerHTML = 'My button'
     document.body.appendChild(buttonEl)
+
+    const { getByTestId } = within(buttonEl.shadowRoot)
+    button = getByTestId('button')
   })
 
   it('has correct defaults', () => {
-    const { getByTestId } = within(buttonEl.shadowRoot)
-    const button = getByTestId('button')
-
     expect(button.classList.contains('default')).toBeTruthy()
     expect(button.type).toBe('button')
   })
 
   it('has correct primary styling', () => {
     buttonEl.setAttribute('kind', 'primary')
-    const { getByTestId } = within(buttonEl.shadowRoot)
-    const button = getByTestId('button')
-
     expect(button.classList.contains('primary')).toBeTruthy()
   })
 
   it('disabled buttons have disabled attribute', () => {
     buttonEl.setAttribute('disabled', '')
-    const { getByTestId } = within(buttonEl.shadowRoot)
-    const button = getByTestId('button')
-
     expect(button.getAttribute('disabled')).toBe('')
   })
 })

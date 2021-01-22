65e22221418319ccba5360187484690e79087adf
Jens-Christian Bjerkek <jens-christian.bjerkek@sparebank1.no>
2021-01-22 19:50:35 +0100

    First working shadow dom testing

--- packages/button/button.test.js
+++ packages/button/button.test.js
@@ -1,72 +1,37 @@
-// https://testing-library.com/docs/dom-testing-library/example-intro
-
-
-
-// ======================================================
-// import { Button } from './button'
-
-// describe('Button', () => {
-//   beforeEach(() => {
-//     window.customElements.define('ui-button', Button)
-//   })
-
-//   it('test 1', () => {
-//     const customElement = document.createElement('ui-button')
-
-//     expect(customElement.shadowRoot.querySelector('button').innerHTML).toBe('My Custom Title')
-//   })
-// })
-// ======================================================
-// ======================================================
+import { within } from '@testing-library/dom'
 import { Button } from './button'
 
 describe('Button', () => {
   let buttonEl
-  let shadowRoot
 
-  beforeEach(() => {
+  beforeAll(() => {
     window.customElements.define('ui-button', Button)
     buttonEl = document.createElement('ui-button')
-    buttonEl.innerText = 'hei'
-    shadowRoot = buttonEl.shadowRoot
-    document.body.append(buttonEl)
+    buttonEl.innerHTML = 'My button'
+    document.body.appendChild(buttonEl)
   })
 
-  it('kjhkjhkjh', () => {
-    expect(shadowRoot.querySelector('button')).toBeTruthy()
+  it('has correct defaults', () => {
+    const { getByTestId } = within(buttonEl.shadowRoot)
+    const button = getByTestId('button')
+
+    expect(button.classList.contains('default')).toBeTruthy()
+    expect(button.type).toBe('button')
   })
 
-  it('test 2', () => {
-    expect(shadowRoot.querySelector('button').innerHTML).toBe('My Custom Title')
+  it('has correct primary styling', () => {
+    buttonEl.setAttribute('kind', 'primary')
+    const { getByTestId } = within(buttonEl.shadowRoot)
+    const button = getByTestId('button')
+
+    expect(button.classList.contains('primary')).toBeTruthy()
+  })
+
+  it('disabled buttons have disabled attribute', () => {
+    buttonEl.setAttribute('disabled', '')
+    const { getByTestId } = within(buttonEl.shadowRoot)
+    const button = getByTestId('button')
+
+    expect(button.getAttribute('disabled')).toBe('')
   })
 })
-// ======================================================
-// import Button from './button'
-
-// const button = new Button()
-// document.body.appendChild(button) // needed for running connectedCallback
-
-// const buttonEl = button.shadowRoot.querySelector('button')
-
-// it('displays Button', () => {
-//   expect(buttonEl.innerHTML.trim()).toBe('It works')
-// })
-
-// import Button from './button'
-
-// describe('Button integration tests', () => {
-//   let element, shadowRoot
-
-//   beforeEach(() => {
-//     window.customElements.define('ui-button', Button)
-//     element = document.createElement('ui-button')
-//     shadowRoot = element.shadowRoot
-//     document.body.append(element)
-//   })
-
-//   describe('init', () => {
-//     it('should add a button tag', () => {
-//       expect(shadowRoot.querySelector('button')).toBeTruthy()
-//     })
-//   })
-// })

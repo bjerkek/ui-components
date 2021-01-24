e27440043f69afee13b3918ff3b6e9aa038440c9
Jens-Christian Bjerkek <jens-christian.bjerkek@sparebank1.no>
2021-01-24 16:08:24 +0100

    wip

--- examples/cra/src/App.js
+++ examples/cra/src/App.js
@@ -4,6 +4,7 @@ import '@ui-components/amount-input'
 import '@ui-components/number-input'
 import '@ui-components/text-input'
 import '@ui-components/checkbox'
+import '@ui-components/date-input'
 
 function App() {
   const onClick = () => {
@@ -37,6 +38,10 @@ function App() {
       <br />
       <br />
       <ui-checkbox>Label</ui-checkbox>
+      <br />
+      <br />
+      <br />
+      <ui-date-input showpicker locale="no" />
     </div>
   );
 }

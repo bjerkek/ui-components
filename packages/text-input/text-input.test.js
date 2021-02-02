6e254b3062c4ac83950f67499a2bb327c4b553d7
Jens-Christian Bjerkek <jens-christian.bjerkek@sparebank1.no>
2021-02-02 20:39:06 +0100

    Testing attributes

--- packages/text-input/text-input.test.js
+++ packages/text-input/text-input.test.js
@@ -20,10 +20,37 @@ describe('Number input', () => {
     expect(input.value).toBe('bacon123eggs¡@£$½¥{[]}±')
   })
 
+  it('should set attribute', () => {
+    textInputEl.setAttribute('arialabel', 'This is aria-label')
+    expect(input.getAttribute('aria-label')).toBe('This is aria-label')
+
+    textInputEl.setAttribute('arialabelledby', 'This is aria-labelledby')
+    expect(input.getAttribute('aria-labelledby')).toBe('This is aria-labelledby')
+
+    textInputEl.setAttribute('placeholder', 'This is placeholder')
+    expect(input.getAttribute('placeholder')).toBe('This is placeholder')
+
+    textInputEl.setAttribute('autocomplete', 'This is autocomplete')
+    expect(input.getAttribute('autocomplete')).toBe('This is autocomplete')
+
+    textInputEl.setAttribute('maxlength', 'This is maxlength')
+    expect(input.getAttribute('maxlength')).toBe('This is maxlength')
+
+    textInputEl.setAttribute('minlength', 'This is minlength')
+    expect(input.getAttribute('minlength')).toBe('This is minlength')
+
+    textInputEl.setAttribute('readonly', 'true')
+    expect(input.hasAttribute('readonly')).toBeTruthy()
+
+    textInputEl.setAttribute('spellcheck', 'true')
+    expect(input.hasAttribute('spellcheck')).toBeTruthy()
+  })
+
   it('should show error message', () => {
     textInputEl.setAttribute('errormessage', 'Something is wrong')
     const { getByTestId } = within(textInputEl.shadowRoot)
     errorContainer = getByTestId('errorContainer')
     expect(errorContainer).toBeTruthy()
+    expect(errorContainer.innerText).toBe('Something is wrong')
   })
 })

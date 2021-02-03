5a1c24c1a54d88e28e55370f38a42aa7964d52e0
Jens-Christian Bjerkek <jens-christian.bjerkek@sparebank1.no>
2021-02-03 14:43:40 +0100

    wip

--- packages/radio-button/radio-group.test.js
+++ packages/radio-button/radio-group.test.js
@@ -1,4 +1,4 @@
-import { within } from '@testing-library/dom'
+import { within, screen } from '@testing-library/dom'
 import { RadioGroup, tagName } from './radio-group'
 
 describe('Radio group', () => {
@@ -8,8 +8,19 @@ describe('Radio group', () => {
   beforeAll(() => {
     window.customElements.define(tagName, RadioGroup)
     radioGroupEl = document.createElement(tagName)
+
+    // radioGroupEl.appendChild() = `
+    //   <ui-radio-button value="one">Number one</ui-radio-button>
+    //   <ui-radio-button value="two">Number two</ui-radio-button>
+    //   <ui-radio-button value="three">Number three</ui-radio-button>
+    // `
     document.body.appendChild(radioGroupEl)
   })
+
+  // it('should render three inputs type radio', () => {
+  //   console.log(radioGroupEl.shadowRoot.innerHTML)
+  //   expect(screen.findByText('Number one')).toBeTruthy()
+  // })
 
   it('should show error message', () => {
     radioGroupEl.setAttribute('errormessage', 'Something is wrong')
